144844bacadc80cd85855897f18f04d38a061909
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-09-28 12:39:27 -0700

    created random choice for AI -D&B

--- AI.py
+++ AI.py
@@ -6,5 +6,7 @@ class AI(Player):
     def __init__(self):
         self.name = random.choice(["John", "Joe", "Jack", "Joseph", "Jordan", "James", "Dan"])
         super().__init__()
-    
-    
+
+    def select_choice(self):
+        self.choice = random.choice(self.options)
+        
