2f05a84fcc562c2cb1a17ecfe5c3ee573df9616e
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-09-28 11:44:32 -0700

    Created the Game Class and included intro/rules methods -David & Ben

---
+++ game.py
@@ -0,0 +1,16 @@
+
+class Game:
+    def __init__(self):
+
+        super().__init__()
+    
+    def intro(self):
+        print()
+        print("Welcome to Rock, Paper, Scissors, Lizard, Spock")
+        print()
+    
+    def rules(self):
+        print("Each player selects an option from the given list. Each option will beat another option, lose against another option, or tie against the same option")
+        print("Here are the different outcomes")
+        print("Rock crushes Scissors \nScissors cuts Paper \nPaper covers Rock \nRock crushes Lizard \nLizard poisons Spock \nSpock smashes Scissors \nScissors decapitates Lizard \nLizard eats Paper \nPaper disproves Spock \nSpock vaporizes Rock")
+
